c6ff670a4d990bf5f6494684973a443556080758
VanTT-BraveBits <vantt@bravebits.vn>
2022-10-05 20:57:22 +0700

    Improvement UI

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const chessboard = document.getElementById("chessboard");
-const ghost = document.getElementById("ghost");
-const placeholder = document.getElementById("placeholder");
+const chessboard = document.getElementById('chessboard');
+const ghost = document.getElementById('ghost');
+const placeholder = document.getElementById('placeholder');
 
 let innerGhost = null;
 let isReadyToDrag = false;
@@ -10,39 +10,39 @@ let finishRow = null;
 let finishColumn = null;
 
 const mouseDown = (e) => {
-    if(e.target.matches('.square') && e.target.innerText !== ''){
+    if (e.target.matches('.square') && e.target.innerText !== '') {
         console.log(e.target.innerText);
         startRow = e.target.dataset.row;
         startColumn = e.target.dataset.column;
         innerGhost = e.target.innerHTML;
         isReadyToDrag = true;
     }
-}
+};
 
 const mouseMove = (e) => {
-    if(isReadyToDrag){
-        ghost.style.display = "block";
+    if (isReadyToDrag) {
+        ghost.style.display = 'block';
         ghost.innerHTML = innerGhost;
 
         const startIndex = document.querySelector(`[data-row="${startRow}"][data-column="${startColumn}"]`);
-        startIndex.innerHTML = "";
-        ghost.style.top = e.clientY - 40 + "px";
-        ghost.style.left = e.clientX - 40 + "px";
+        startIndex.innerHTML = '';
+        ghost.style.top = e.clientY - 40 + 'px';
+        ghost.style.left = e.clientX - 40 + 'px';
         // console.log(e.target.dataset.row, e.target.dataset.column);
     }
-}
+};
 
 const mouseUp = (e) => {
-    if(isReadyToDrag){
+    if (isReadyToDrag) {
         isReadyToDrag = false;
-        ghost.style.display = "none";
+        ghost.style.display = 'none';
         finishRow = e.target.dataset.row;
         finishColumn = e.target.dataset.column;
         const finishIndex = document.querySelector(`[data-row="${finishRow}"][data-column="${finishColumn}"]`);
         finishIndex.innerHTML = innerGhost;
         console.log(finishRow, finishColumn);
     }
-}
+};
 chessboard.addEventListener('mousedown', (e) => mouseDown(e));
 chessboard.addEventListener('mousemove', (e) => mouseMove(e));
 chessboard.addEventListener('mouseup', (e) => mouseUp(e));
